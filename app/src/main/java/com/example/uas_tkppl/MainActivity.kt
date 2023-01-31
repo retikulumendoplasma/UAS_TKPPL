d4ca67ee844ee71d8d7a4caf67d806dc64fa85ff
ozoraoraora <nonamegaming743@gmail.com>
2023-01-31 19:32:06 +0700

    fix ukuran edit text dan fix action bar

--- app/src/main/java/com/example/uas_tkppl/MainActivity.kt
+++ app/src/main/java/com/example/uas_tkppl/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.uas_tkppl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         //title action bar
         setTitle("BMI CALCULATOR")
 
+    }
+
+    //setting_untuk_dark_dan_light_mode
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.menu, menu)
+
+        return true
     }
 }
