c38755290e50d4063c20072c549a60e8d20e97fb
retikulumendoplasma <Pranatayoga07@gmail.com>
2023-02-01 13:11:19 +0700

    update variabel untuk tv_hasil dan tambah activity_recomendation

---
+++ app/src/main/java/com/example/uas_tkppl/Recomendation.kt
@@ -0,0 +1,13 @@
+package com.example.uas_tkppl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recomendation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recomendation)
+
+
+    }
+}
--- app/src/main/java/com/example/uas_tkppl/Result.kt
+++ app/src/main/java/com/example/uas_tkppl/Result.kt
@@ -8,6 +8,9 @@ class Result : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val gemuk = "Nilai indeks massa tubuh (IMT) di atas 25.0 Anda dianggap memiliki kelebihan berat badan"
+        val normal= "Nilai indeks massa tubuh Anda 18.5 sampai 24.9, artinya Anda memiliki berat badan normal"
+        val kurus = "Nilai indeks massa tubuh (IMT) di bawah 18,5 menandakan berat badan Anda kurang"
 
         val berat = intent.getIntExtra("berat",0)
         val tinggi = intent.getIntExtra("tinggi",0)
