ae95c9e3168cafc86cd037ddc10996c29f8b1704
ozoraoraora <nonamegaming743@gmail.com>
2023-02-01 16:15:06 +0700

    membuat gambaran kasar template untuk halaman rekomendation, dan pembuatan dark mode dan light mode

--- app/src/main/java/com/example/uas_tkppl/Recomendation.kt
+++ app/src/main/java/com/example/uas_tkppl/Recomendation.kt
@@ -2,11 +2,22 @@ package com.example.uas_tkppl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.tugas_tkkpl_e_call.helper.constant
+import com.example.tugas_tkkpl_e_call.helper.preferencesHelper
 
 class Recomendation : AppCompatActivity() {
+    lateinit var sharedPref: preferencesHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recomendation)
+
+
+        sharedPref = preferencesHelper(this)
+
+        if (sharedPref.getBoolean(constant.PREF_ISON) == true) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
 
 
     }
